0aec7c128d11618c802995465d66972fe6eeced8
[dyad] <jonyrabuansyah56@guru.smk.belajar.id>
2025-08-07 19:43:14 +0700

    [dyad] wrote 1 file(s)

---
+++ .pnpmfile.cjs
@@ -0,0 +1,12 @@
+function readPackage(pkg) {
+  // This hook is used to modify the package.json of a dependency before it is used.
+  // In this case, we don't need to modify anything. The presence of this hook
+  // is enough to signal to pnpm on Vercel that it should process scripts.
+  return pkg;
+}
+
+module.exports = {
+  hooks: {
+    readPackage,
+  },
+};
